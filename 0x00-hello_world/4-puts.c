a0c48d6b0a657a748864c148382a98f9feba93e0
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2023-10-05 17:44:53 -0700

    Size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/**
+ /**
  * main -this is the main function
  *
  * Return: always 0
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+       	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
