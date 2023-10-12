0986f54ba86602ed9ad7e7c6c4e5bcb3af73db3e
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2023-10-11 17:09:41 -0700

    Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+ #include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: parameter to be checked
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (0);
+	else
+		return (0);
+}
