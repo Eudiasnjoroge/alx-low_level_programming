19dc004160d14261386bffa45051a514f76a76ae
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-06-27 13:56:55 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,17 +8,20 @@
 
 void print_triangle(int size)
 {
+	int i, j, k;
+
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		int i;
-		int j;
-
 		for (i = 1 ; i <= size; i++)
 		{
-			for (j = size - i ; j > 0 ; j--)
+			for (j = i ; j < size ; j++)
+			{
 				_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
