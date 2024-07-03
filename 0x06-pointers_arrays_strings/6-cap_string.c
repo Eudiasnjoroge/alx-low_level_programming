5cd9f85b5aff2c10f95777143c2c9749056d90d0
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-07-03 03:56:50 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,32 @@
+#include <stdio.h>
+
+/**
+ * is_lower - Checks if a character is a lowercase letter
+ * @c: The character to check
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise
+ */
+int is_lower(char c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+/**
+ * to_upper - Converts a lowercase letter to uppercase
+ * @c: The character to convert
+ *
+ * Return: The uppercase equivalent if c is lowercase, otherwise c
+ */
+char to_upper(char c)
+{
+	if (is_lower(c))
+	{
+		return c - 'a' + 'A';
+	}
+
+	return c;
+}
+
 #include "main.h"
 
 /**
@@ -8,29 +37,37 @@
 
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i = 0;
 
-	while (str[index])
+	if (str[i] != '\0' && is_lower(str[i]))
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-		if (str[index - 1] == ' ' ||
-		str[index - 1] == '\t' ||
-		str[index - 1] == '\n' ||
-		str[index - 1] == ',' ||
-		str[index - 1] == ';' ||
-		str[index - 1] == '.' ||
-		str[index - 1] == '!' ||
-		str[index - 1] == '?' ||
-		str[index - 1] == '-' ||
-		str[index - 1] == '(' ||
-		str[index - 1] == ')' ||
-		str[index - 1] == '{' ||
-		str[index - 1] == '}' ||
-		index == 0)
-		str[index] -= 32;
-		index++;
+		str[i] = to_upper(str[i]);
 	}
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' ||
+		str[i] == '\t' ||
+		str[i] == '\n' ||
+		str[i] == ',' ||
+		str[i] == ';' ||
+		str[i] == '.' ||
+		str[i] == '!' ||
+		str[i] == '?' ||
+		str[i] == '"' ||
+		str[i] == '(' ||
+		str[i] == ')' ||
+		str[i] == '{' ||
+		str[i] == '}')
+		{
+			if (str[i + 1] != '\0' && is_lower(str[i + 1]))
+			{
+				str[i + 1] = to_upper(str[i + 1]);
+			}
+		}
+		i++;
+	}
+
 	return (str);
 }
 
