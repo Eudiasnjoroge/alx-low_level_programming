8ff5a734eed5855c7729f650de9f2b1418a81199
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-07-15 03:23:49 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * str_conat - concatenates two strings of any size
+ * str_concat - concatenates two strings of any size
  * @s1: the first string to concatenate
  * @s2: the second string to concatenate
  *
