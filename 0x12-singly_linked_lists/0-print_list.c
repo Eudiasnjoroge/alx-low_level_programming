9b9bf1dac690ed555bbca90111e338ae322db310
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-08-01 18:25:49 +0300

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,13 +2,6 @@
 #include <stdio.h>
 #include <stddef.h>
 
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-}list_t;
-
 /**
  * print_list - prints all elements of a list_t list
  * @h: pointer to the head of the list_t list
