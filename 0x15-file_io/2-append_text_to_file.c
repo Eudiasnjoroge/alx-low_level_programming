b8d6707efd36f17672c2a98bdf9a4a86cbee6c81
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-08-14 17:00:12 +0300

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,14 +15,14 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, length = 0, bytes_written;
+	mode_t new_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-	{
 		return (-1);
-	}
+
 	if (text_content != NULL)
 	{
 		while (text_content[length])
@@ -34,7 +34,12 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
+	if (fchmod(fd, new_mode) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 	close(fd);
-	return (-1);
+	return (1);
 }
 
